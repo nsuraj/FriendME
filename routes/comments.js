2d2f4c9e2aef540356f50e48c7f713c6fb30bd9f
suraj <nsurajrajan@gmail.com>
2020-11-26 19:50:22 +0530

    basic functionality

--- routes/comments.js
+++ routes/comments.js
@@ -1,22 +1,11 @@
 const express = require('express');
-
 const router = express.Router();
-const homeController = require('../controllers/home_controller');
+const passport = require('passport');
 
-console.log('router loaded');
+const commentsController = require('../controllers/comments_controller');
 
-
-router.get('/', homeController.home);
-router.use('/users', require('./users'));
-router.use('/posts', require('./posts'));
-router.use('/comments', require('./comments'));
-router.use('/likes', require('./likes'));
-
-
-router.use('/api', require('./api'));
-
-// for any further routes, access from here
-// router.use('/routerName', require('./routerfile));
+router.post('/create', passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 
 module.exports = router;
